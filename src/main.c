e88e74bf111da73f09537acf8d4114100a166f7d
emilio <emilio@crunchbanguco.(none)>
2013-07-01 19:05:13 +0200

    Adding nicer first lines from Belka in main.c

--- src/main.c
+++ src/main.c
@@ -803,14 +803,25 @@ int main()
     MOVE theBest;
     int movecnt;
 
-    puts ("Kitteneitor, by Emilio Diaz");
-    puts (" Help");
-    puts (" d: display board");
-    puts (" MOVE: make a move (e.g. b1c3, a7a8q, e1g1)");
-    puts (" on: force computer to move");
-    puts (" quit: exit");
-    puts (" sd n: set engine depth to n plies");
-    puts (" undo: take back last move");
+//    puts ("Kitteneitor, by Emilio Diaz");
+//    puts (" Help");
+//    puts (" d: display board");
+//    puts (" MOVE: make a move (e.g. b1c3, a7a8q, e1g1)");
+//    puts (" on: force computer to move");
+//    puts (" quit: exit");
+//    puts (" sd n: set engine depth to n plies");
+//    puts (" undo: take back last move");
+
+    /* Belka */
+    puts (" \n Kitteneitor version June 27th 2013 by Emilio Diaz \n =================================================\n\n");
+    puts (" Help overview:");
+    puts (" making a move: e.g. e2e4, c7c5, a7a8q, e1g1 etc.");
+    puts (" d ............ displaying current board");
+    puts (" on ........... forcing the engine to move");
+    puts (" sd <n> ....... setting depth to <n> plies");
+    puts (" undo ......... taking back last move (ply)");
+    puts (" quit ......... quit console application \n\n");
+    /* Belka */
 
     side = WHITE;
     computer_side = BLACK;	/* Human is white side */
